9ff708cafce3f144fd3ba8eb51d3527cbfc686a8
vicctorneves <vicctorneves88@gmail.com>
2022-06-30 10:05:38 -0300

    atualização tabuada

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-const numeroTabuada = prompt('Tabuada do número: ');
-const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
-const tabuada = document.querySelector('#tabuada');
+const showTabuada = document.querySelector('#show-tabuada');
+showTabuada.addEventListener('click', function (){
+   const numeroTabuada = document.querySelector('#numero').value;
+   const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
+   const tabuada = document.querySelector('#tabuada');
+   tabuada.innerHTML = '';
+   calcularTabuada(numeroTabuada, tabuadaTitle);
+})
 
-for (let i = 0; i <= 10; i++) {
-   let result = numeroTabuada * i;
-   tabuada.innerHTML += (`<p>${numeroTabuada} X ${i} = ${result}</p>`);
-   tabuadaTitle.innerHTML = `do ${i}`;
-}
+function calcularTabuada(numeroTabuada, tabuadaTitle) {
+   for (let i = 0; i <= 10; i++) {
+      tabuadaTitle.innerHTML = `do ${numeroTabuada}`;
+      let result = numeroTabuada * i;
+      tabuada.innerHTML += (`<p>${numeroTabuada} X ${i} = ${result}</p>`);
+   }
+};
+
+
+
 
 
