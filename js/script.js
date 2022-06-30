8690f9866c424e9fb406cdc100abfc9fc14eff89
vicctorneves <vicctorneves88@gmail.com>
2022-06-30 11:50:02 -0300

    width and height

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const showTabuada = document.querySelector('#show-tabuada');
-showTabuada.addEventListener('click', function (){
+const btnShowTabuada = document.querySelector('#btn-show-tabuada');
+
+btnShowTabuada.addEventListener('click', function (){
    const numeroTabuada = document.querySelector('#numero').value;
    const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
    const tabuada = document.querySelector('#tabuada');
    tabuada.innerHTML = '';
    calcularTabuada(numeroTabuada, tabuadaTitle);
 })
-
 function calcularTabuada(numeroTabuada, tabuadaTitle) {
    for (let i = 0; i <= 10; i++) {
       tabuadaTitle.innerHTML = `do ${numeroTabuada}`;
