d24a8749d6dcd0f99d1e3d66d0f50be624158b30
vicctorneves <vicctorneves88@gmail.com>
2022-06-30 12:32:30 -0300

    tabuada 100%

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 const btnShowTabuada = document.querySelector('#btn-show-tabuada');
+const tabuada = document.querySelector('#tabuada');
+const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
 
 btnShowTabuada.addEventListener('click', function (){
-   const numeroTabuada = document.querySelector('#numero').value;
-   const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
-   const tabuada = document.querySelector('#tabuada');
+   const numberInput = document.querySelector('#number').value;
+   console.log(numberInput);
+   resetTabuada();
+   showTabuadaTitle(numberInput);
+   calcularTabuada(numberInput, tabuadaTitle);
+});
+function resetTabuada(){
    tabuada.innerHTML = '';
-   calcularTabuada(numeroTabuada, tabuadaTitle);
-})
-function calcularTabuada(numeroTabuada, tabuadaTitle) {
+};
+function showTabuadaTitle(numberInput){
+   tabuadaTitle.innerHTML = `do ${numberInput}`;
+}
+function calcularTabuada(numberInput) {
    for (let i = 0; i <= 10; i++) {
-      tabuadaTitle.innerHTML = `do ${numeroTabuada}`;
-      let result = numeroTabuada * i;
-      tabuada.innerHTML += (`<p>${numeroTabuada} X ${i} = ${result}</p>`);
+      let result = numberInput * i;
+      tabuada.innerHTML += (`<p>${numberInput} X ${i} = ${result}</p>`);
    }
 };
 
