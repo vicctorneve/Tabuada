6b9f53ce638bccedff5d6564d1cac3ca5475e61c
vicctorneves <vicctorneves88@gmail.com>
2022-07-19 10:43:19 -0300

    📚 docs: Atualizaçao do README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Tabuada
- Tabuada criada afim de estudo pratico de Javascript
+<p> Tabuada criada afim de estudo pratico de Javascript.</p>
+
+<img src='assets/img/preview-tabuada.png' alt='preview do site de tabuada'>
+
+
+## Tecnologias:
+
+<ul>
+   <li><a href="https://www.w3schools.com/html/">HTML</a>
+   <li><a href="https://www.w3schools.com/css/">CSS</a>
+   <li><a href="https://www.w3schools.com/js/">JS</a>
+</ul>
+
+### Status:
+<p> Concluido mas sujeito a futuras atualizações. ✔️</p>
--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
+const tabuadaTitle = document.querySelector('#tabuada-do-numero');
+let tabuada = document.querySelector('#tabuada');
 const btnShowTabuada = document.querySelector('#btn-show-tabuada');
-const tabuada = document.querySelector('#tabuada');
-const tabuadaTitle = document.querySelector('span#tabuada-do-numero');
+const inputNumber = document.querySelector('#number-input')
 
-btnShowTabuada.addEventListener('click', function (){
-   const numberInput = document.querySelector('#number').value;
-   resetTabuada();
-   showTabuadaTitle(numberInput);
-   calcularTabuada(numberInput, tabuadaTitle);
-});
-function resetTabuada(){
-   tabuada.innerHTML = '';
-};
-function showTabuadaTitle(numberInput){
-   tabuadaTitle.innerHTML = `do ${numberInput}`;
+btnShowTabuada.addEventListener('click', execute)
+function execute() {
+   if (!inputNumber.value) return
+   calcTabuada()
+   showNumberTitle();
 }
-function calcularTabuada(numberInput) {
+document.addEventListener('keypress', function(e) {
+   if (e.key === "Enter"){
+      if (!inputNumber.value) return
+      execute();
+   }
+})
+// function showTabuada (result) {
+//    tabuada.innerHTML = '';
+//    let p = createP();
+//    p.innerText = result;
+//    console.log(p)
+//    tabuada.appendChild(p)
+// }
+function createP() {
+   const p = document.createElement('p');
+   return p
+}
+function showNumberTitle() {
+   tabuadaTitle.innerHTML=`do ${inputNumber.value}`;
+}
+function calcTabuada () {
+   tabuada.innerHTML = '';
    for (let i = 0; i <= 10; i++) {
-      let result = numberInput * i;
-      tabuada.innerHTML += (`<p>${numberInput} X ${i} = ${result}</p>`);
+      const number = inputNumber.value;
+      let resultCalc = number * i;
+      let result = ` ${number} X ${i} = ${resultCalc} `
+      let p = createP()
+      p.innerText = result
+      tabuada.appendChild(p)
+      // showTabuada(result)
    }
 };
-
-
-
-
 
